b76176a17edbcc1dcbcc36f29c0975ddfcfb9689
Simon Urbanek <simon.urbanek@r-project.org>
2008-07-15 15:11:32 +0000

    clean up tests
    git-svn-id: svn://svn.rforge.net/org/trunk/rosuda/REngine@2910 955e8658-2bef-402f-a597-8b5c18d8b76d

--- Rserve/test/test.java
+++ Rserve/test/test.java
@@ -13,16 +13,17 @@ public class test {
 	    System.out.println(">>"+c.eval("R.version$version.string").asString()+"<<");
 
 		{
-			System.out.println("Test string retrieval...");
+			System.out.println("* Test string and list retrieval");
 			RList l = c.eval("{d=data.frame(\"huhu\",c(11:20)); lapply(d,as.character)}").asList();
 			int cols = l.size();
 			int rows = l.at(0).length();
 			String[][] s = new String[cols][];
 			for (int i=0; i<cols; i++) s[i]=l.at(i).asStrings();
+			System.out.println("PASSED");
 		}
 		
 	    {
-		System.out.println("Test NA/NaN support in double vectors...");
+		System.out.println("* Test NA/NaN support in double vectors...");
 		double R_NA = Double.longBitsToDouble(0x7ff00000000007a2L);
 		// int R_NA_int = -2147483648; // just for completeness
 		double x[] = { 1.0, 0.5, R_NA, Double.NaN, 3.5 };
@@ -31,81 +32,95 @@ public class test {
 		System.out.println(nas);
 		if (!nas.equals("[1] 1.0 0.5  NA NaN 3.5"))
 		    throw new TestException("NA/NaN assign+retrieve test failed");
+		System.out.println("PASSED");
 	    }
 		
 	    {
-			System.out.println("Test assigning of lists and vectors ...");
+			System.out.println("* Test assigning of lists and vectors ...");
 			RList l = new RList();
 			l.put("a",new REXPInteger(new int[] { 0,1,2,3}));
 			l.put("b",new REXPDouble(new double[] { 0.5,1.2,2.3,3.0}));
-			System.out.println("assign x=pairlist, y=vector, z=data.frame");
+			System.out.println("  assign x=pairlist");
 			c.assign("x", new REXPList(l));
+			System.out.println("  assign y=vector");
 			c.assign("y", new REXPGenericVector(l));
+			System.out.println("  assign z=data.frame");
 			c.assign("z", REXP.createDataFrame(l));
-			System.out.println("pull all three back to Java");
+			System.out.println("  pull all three back to Java");
 			REXP x = c.parseAndEval("x");
-			System.out.println("x = "+x);
+			System.out.println("  x = "+x);
 			x = c.eval("y");
-			System.out.println("y = "+x);
+			System.out.println("  y = "+x);
 			x = c.eval("z");
-			System.out.println("z = "+x);
+			System.out.println("  z = "+x);
+			System.out.println("PASSED");
 	    }
 
             { // factors
-                System.out.println("test support of factors");
+                System.out.println("* Test support of factors");
                 REXP f = c.parseAndEval("factor(paste('F',as.integer(runif(20)*5),sep=''))");
-                System.out.println("isFactor: "+f.isFactor()+"\nasFactor: "+f.asFactor());
+				System.out.println("  f="+f);
+                System.out.println("  isFactor: "+f.isFactor()+", asFactor: "+f.asFactor());
                 if (!f.isFactor() || f.asFactor() == null) throw new TestException("factor test failed");
-                System.out.println("singe-level factor used to degenerate:");
+                System.out.println("  singe-level factor used to degenerate:");
                 f = c.parseAndEval("factor('foo')");
-                System.out.println("isFactor: "+f.isFactor()+"\nasFactor: "+f.asFactor());
+                System.out.println("  isFactor: "+f.isFactor()+", asFactor: "+f.asFactor());
                 if (!f.isFactor() || f.asFactor() == null) throw new TestException("single factor test failed (not a factor)");
 				if (!f.asFactor().at(0).equals("foo")) throw new TestException("single factor test failed (wrong value)");
+                System.out.println("  test factors with null elements contents:");
 				c.assign("f", new REXPFactor(new RFactor(new String[] { "foo", "bar", "foo", "foo", null, "bar" })));
 				f = c.parseAndEval("f");
                 if (!f.isFactor() || f.asFactor() == null) throw new TestException("factor assign-eval test failed (not a factor)");
-				System.out.println("f = "+f.asFactor());
+				System.out.println("  f = "+f.asFactor());
+				f = c.parseAndEval("as.factor(c(1,'a','b',1,'b'))");
+				System.out.println("  f = "+f);
+                if (!f.isFactor() || f.asFactor() == null) throw new TestException("factor test failed (not a factor)");
+				System.out.println("PASSED");
             }
 
 
 	    {
-			System.out.println("lowess test");
+			System.out.println("* Lowess test");
 			double x[] = c.eval("rnorm(100)").asDoubles();
 			double y[] = c.eval("rnorm(100)").asDoubles();
 			c.assign("x", x);
 			c.assign("y", y);
 			RList l = c.parseAndEval("lowess(x,y)").asList();
-			System.out.println(l);
+			System.out.println("  "+l);
 			x = l.at("x").asDoubles();
 			y = l.at("y").asDoubles();
+			System.out.println("PASSED");
 		}
 
 	    {
-		// multi-line expressions
-		System.out.println("Test multi-line expressions");
-		if (c.eval("{ a=1:10\nb=11:20\nmean(b-a) }\n").asInteger()!=10)
-		    throw new TestException("multi-line test failed.");
+			// multi-line expressions
+			System.out.println("* Test multi-line expressions");
+			if (c.eval("{ a=1:10\nb=11:20\nmean(b-a) }\n").asInteger()!=10)
+				throw new TestException("multi-line test failed.");
+			System.out.println("PASSED");
 	    }
 		{
-            System.out.println("matrix: create a matrix");
+            System.out.println("* Matrix tests\n  matrix: create a matrix");
             int m=100, n=100;
             double[] mat=new double[m*n];
             int i=0;
             while (i<m*n) mat[i++]=i/100;
-            System.out.println("matrix: assign a matrix");
+            System.out.println("  matrix: assign a matrix");
             c.assign("m", mat);
             c.voidEval("m<-matrix(m,"+m+","+n+")");
             System.out.println("matrix: cross-product");
             double[][] mr=c.parseAndEval("crossprod(m,m)").asDoubleMatrix();
+			System.out.println("PASSED");
 		}
 		
 		{
-			System.out.println("test serialization and raw vectors");
+			System.out.println("* Test serialization and raw vectors");
 			byte[] b = c.eval("serialize(ls, NULL, ascii=FALSE)").asBytes();
-			System.out.println("serialized ls is "+b.length+" bytes long");
+			System.out.println("  serialized ls is "+b.length+" bytes long");
 			c.assign("r", new REXPRaw(b));
 			String[] s = c.eval("unserialize(r)()").asStrings();
-			System.out.println("we have "+s.length+" items in the workspace");
+			System.out.println("  we have "+s.length+" items in the workspace");
+			System.out.println("PASSED");
 		}
 		
 		} catch (RserveException rse) {
